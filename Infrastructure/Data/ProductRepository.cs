f56a4fa667e78f0d5e9c82958855de69c51da5f9
محمد شربجي <Mohammadshrbje3@gmail.com>
2023-02-12 11:52:36 +0300

    the fisrt part of repository

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<IReadOnlyList<Product>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
